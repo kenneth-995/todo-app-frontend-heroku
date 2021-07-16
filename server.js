443505038693c6913193e73c576e7e6c7b5306e4
kenneth-ferre <kenneth.ferre@gmail.com>
2021-07-16 18:23:17 +0200

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 // Serve static files
 app.use(express.static(__dirname + '/dist/ohmycodetodofront'));
+
+app.use(cors());
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
